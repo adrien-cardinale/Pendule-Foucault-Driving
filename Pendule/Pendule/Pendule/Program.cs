5552dea1b9b28dcf8968eeacc4fc298aa65c78a9
Adrien Cardinale <adrien.cardinale@heig-vd.ch>
2023-06-14 17:24:34 +0200

    Pendule prog

---
+++ Pendule/Pendule/Pendule/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ch.etel.edi.dsa.v40;
+using Pendule;
+
+namespace pendule
+{
+    class Pendule
+    {
+        static void Main(string[] args)
+        {
+            Regulateur regulateur = new Regulateur();
+            regulateur.OpenBus();
+            regulateur.Init();
+            regulateur.CloseBus();
+            //regulateur.test();
+        }
+    }
+}
